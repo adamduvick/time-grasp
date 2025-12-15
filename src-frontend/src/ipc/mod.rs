92b44a8090e99cd33e8dda551c5d6fc61a428736
Adam Duvick <adamduvick@gmail.com>
2025-12-15 16:55:58 -0600

    . play around with macros

--- src-frontend/src/ipc/mod.rs
+++ src-frontend/src/ipc/mod.rs
@@ -1,12 +1,43 @@
 mod category;
-mod category_group;
+// mod category_group;
 mod entry;
 mod support;
 
 pub use category::*;
-pub use category_group::*;
+// pub use category_group::*;
 pub use entry::*;
 pub use model::*;
+
+use crate::error::Result;
+
+/// If I have a ton of IPC functions, this macro could help, but it's best to
+/// not do much with it at this point until I prove I need it. There may be
+/// reasons for having other types of IPC calls that do not mirror so closely the
+/// signatures that exist in the backend.
+///
+/// This proof of concept will remain in case it proves useful in the future.
+#[macro_export]
+macro_rules! ipc_call {
+    (pub async fn $name:ident ( $( $arg:ident : $ty:ty ),* $(,)? ) -> $ret:ty ) => {
+        pub async fn $name( $( $arg : $ty ),* ) -> $ret {
+            const CMD: &'static str = stringify!($name);
+
+            #[derive(::serde::Serialize)]
+            struct Args {
+                $( $arg : $ty ),*
+            }
+
+            let wrapped_args = Args { $( $arg ),* };
+            crate::ipc::support::invoke_typed(CMD, wrapped_args).await
+        }
+    };
+}
+
+ipc_call!(pub async fn create_group(data: C_Group) -> Result<Uuid>);
+ipc_call!(pub async fn read_group(id: Uuid) -> Result<R_Group>);
+ipc_call!(pub async fn list_group(filter: CategoryGroupFilter) -> Result<Vec<R_Group>>);
+ipc_call!(pub async fn update_group(data: U_Group) -> Result<Uuid>);
+ipc_call!(pub async fn delete_group(data: D_Group) -> Result<Uuid>);
 
 // TODO remove once tested
 #[derive(serde::Serialize)]
