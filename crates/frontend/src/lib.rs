4230cf520ba4ceacfec9dee8f60984852a1e4958
Adam Duvick <adamduvick@gmail.com>
2025-12-28 21:23:37 -0600

    . refactor workspaces

---
+++ crates/frontend/src/lib.rs
@@ -0,0 +1,19 @@
+#![allow(unused)]
+#![allow(dead_code)]
+#![allow(unused_imports)]
+#![allow(unused_variables)]
+
+pub mod app;
+mod error;
+mod ipc;
+
+use leptos::prelude::*;
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen(start)]
+pub fn start() {
+    console_error_panic_hook::set_once();
+    mount_to_body(|| {
+        view! { <app::App/> }
+    });
+}
--- src-frontend/src/lib.rs
+++
@@ -1,8 +0,0 @@
-#![allow(unused)]
-#![allow(dead_code)]
-#![allow(unused_imports)]
-#![allow(unused_variables)]
-
-pub mod app;
-mod error;
-mod ipc;
