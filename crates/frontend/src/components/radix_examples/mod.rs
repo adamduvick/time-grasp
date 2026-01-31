9acfeab81b80977c985f65cea5c003e3f4fccaf3
Adam Duvick <adamduvick@gmail.com>
2026-01-31 17:38:47 -0600

    update aspect_ratio example

--- crates/frontend/src/components/radix_examples/mod.rs
+++ crates/frontend/src/components/radix_examples/mod.rs
@@ -4,23 +4,36 @@ use radix::AspectRatio;
 #[component]
 pub fn Radix() -> impl IntoView {
     view! {
-        <h2>"16 / 9 Image"</h2>
-        <AspectRatio ratio=(16.0 / 9.0)>
-            <img
-                class="Image"
-                style="object-fit: cover; width: 100%; height: 100%;" // style via Image class
-                src="https://images.unsplash.com/photo-1535025183041-0991a977e25b?w=300&dpr=2&q=80"
-                alt="Landscape photograph by Tobias Tullius"
-            />
-        </AspectRatio>
-        <h2>"9 / 16 Image"</h2>
-        <AspectRatio ratio=(9.0 / 16.0)>
-            <img
-                class="Image"
-                style="object-fit: cover; width: 100%; height: 100%;" // style via Image class
-                src="https://images.unsplash.com/photo-1535025183041-0991a977e25b?w=300&dpr=2&q=80"
-                alt="Landscape photograph by Tobias Tullius"
-            />
-        </AspectRatio>
+        <h2>"Aspect Ratio"</h2>
+        <h3>"16 / 9 Image"</h3>
+        <div
+            style:width="300px"
+            style:border-radius="6px"
+            style:overflow="hidden"
+        >
+            <AspectRatio ratio=(16.0 / 9.0)>
+                <img
+                    class="Image"
+                    style="object-fit: cover; width: 100%; height: 100%;" // style via Image class
+                    src="https://images.unsplash.com/photo-1535025183041-0991a977e25b?w=300&dpr=2&q=80"
+                    alt="Landscape photograph by Tobias Tullius"
+                />
+            </AspectRatio>
+        </div>
+        <h3>"9 / 16 Image"</h3>
+        <div
+            style:width="300px"
+            style:border-radius="6px"
+            style:overflow="hidden"
+        >
+            <AspectRatio ratio=(9.0 / 16.0)>
+                <img
+                    class="Image"
+                    style="object-fit: cover; width: 100%; height: 100%;" // style via Image class
+                    src="https://images.unsplash.com/photo-1535025183041-0991a977e25b?w=300&dpr=2&q=80"
+                    alt="Landscape photograph by Tobias Tullius"
+                />
+            </AspectRatio>
+        </div>
     }
 }
